b23f308118de9d49545d57853fd74f3dad705970
strpc <renat.kruchinskij@gmail.com>
2025-11-07 11:00:05 +0300

    fix: typo

--- src/arch_blueprint/__main__.py
+++ src/arch_blueprint/__main__.py
@@ -11,7 +11,7 @@ def main() -> None:
     parser.add_argument(
         "root",
         type=str,
-        help="Name of root python module in project (example: 'myapp'",
+        help="Name of root python module in project (example: 'myapp')",
     )
     parser.add_argument(
         "--modules",
